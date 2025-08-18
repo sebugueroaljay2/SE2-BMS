7a7c83294cb566e964a9fcae87e55a8089a889d7
sebugueroaljay2 git config --global user.email sebugueroaljay2@example.com <sebugueroaljay10@gmail.com>
2025-08-19 06:51:34 +0800

    images

--- resources/js/Layouts/AnotherLayout.vue
+++ resources/js/Layouts/AnotherLayout.vue
@@ -6,7 +6,7 @@
     <section>
         <div class="flex justify-center py-4 mt-16">
             <!-- <img src="/images/bg1.jpg" alt="bg1" class="w-24 object-cover"> -->
-             <img src="/images/bg1.jpg" alt="bg1" class="w-24 object-cover">
+             <img src="/Images/bg1.jpg" alt="bg1" class="w-24 object-cover">
         </div>
         <div class="grid cols-1 justify-center ">
             <span class="text-2xl font-bold">Welcome to Barangay </span>
