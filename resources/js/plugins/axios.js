91354b34f91cf759eea30e2de66f9e0c13aa9d69
sebugueroaljay2 git config --global user.email sebugueroaljay2@example.com <sebugueroaljay10@gmail.com>
2025-08-19 08:28:04 +0800

    update plugin

--- resources/js/plugins/axios.js
+++ resources/js/plugins/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default {
   install(app) {
     // automatic baseURL depende sa domain kung saan naka-open
-    axios.defaults.baseURL = window.location.origin + "/api";
+    axios.defaults.baseURL = window.location.origin;
     axios.defaults.withCredentials = true;
 
     // para magamit sa components as this.$axios
